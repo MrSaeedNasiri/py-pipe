b03b2587b7a634fc63b43880f1383f3f803a1740
MrSaeedNasiri <mr.nasiri.saeed@gmail.com>
2022-08-28 10:52:48 +0430

    black styles

--- py-pipe.py
+++ py-pipe.py
@@ -3,39 +3,61 @@ import subprocess
 import shlex
 
 project_repo = "CHANGE_ME"
-project_dir  = "/CHANGE/ME"
+project_dir = "/CHANGE/ME"
+
 
 def clone():
-    subprocess.run(shlex.split("git clone https://github.com/..."), cwd='/home/ubuntu/')
+    subprocess.run(shlex.split("git clone https://github.com/..."), cwd="/home/ubuntu/")
+
 
 def pull():
     subprocess.run(shlex.split("git pull"), cwd=project_dir)
 
+
 def local_commit():
-    return subprocess.run(shlex.split("git rev-parse main"), cwd=project_dir,capture_output=True).stdout.decode("utf-8").rstrip()
+    return (
+        subprocess.run(
+            shlex.split("git rev-parse main"), cwd=project_dir, capture_output=True
+        )
+        .stdout.decode("utf-8")
+        .rstrip()
+    )
+
 
 def remote_commit():
-    ps = subprocess.Popen(["git","ls-remote",project_repo,"refs/heads/main"], cwd=project_dir,stdout=subprocess.PIPE)
-    output = subprocess.check_output(["cut","-f","1"],stdin=ps.stdout).decode("utf-8").rstrip()
+    ps = subprocess.Popen(
+        ["git", "ls-remote", project_repo, "refs/heads/main"],
+        cwd=project_dir,
+        stdout=subprocess.PIPE,
+    )
+    output = (
+        subprocess.check_output(["cut", "-f", "1"], stdin=ps.stdout)
+        .decode("utf-8")
+        .rstrip()
+    )
     ps.wait()
     return output
 
 
-while(True):
-    print('job is runing')
-    if(local_commit() != remote_commit()):
+while True:
+    print("job is runing")
+    if local_commit() != remote_commit():
         try:
             # TODO: kill started Popen processes
-            subprocess.run(shlex.split("killall node"), cwd=project_dir,capture_output=True)
-            print('current app killed')
+            subprocess.run(
+                shlex.split("killall node"), cwd=project_dir, capture_output=True
+            )
+            print("current app killed")
         except:
-            print('nothing for kill')
+            print("nothing for kill")
         pull()
-        subprocess.run(shlex.split("yarn"), cwd=project_dir,capture_output=True)
-        print('installed')
-        subprocess.run(shlex.split("yarn build:prod"), cwd=project_dir,capture_output=True)
-        print('builded')
+        subprocess.run(shlex.split("yarn"), cwd=project_dir, capture_output=True)
+        print("installed")
+        subprocess.run(
+            shlex.split("yarn build:prod"), cwd=project_dir, capture_output=True
+        )
+        print("builded")
         subprocess.Popen(shlex.split("yarn start:prod"), cwd=project_dir)
-        print('started')
-    print('nothing to do')
+        print("started")
+    print("nothing to do")
     sleep(60)
